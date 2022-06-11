d72dcfea29390b05e3a1a315efca6b1203a9057c
EmmaK <emmaculatewkamau@gmail.com>
2022-06-11 19:00:51 +0300

    Define registration account templates

--- AwwardsClone/urls.py
+++ AwwardsClone/urls.py
@@ -20,7 +20,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('app.urls'))
+    path('',include('awwards.urls'))
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
