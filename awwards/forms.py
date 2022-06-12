531700ff8e41118b7df89f7aa305205436b5b0bc
EmmaK <emmaculatewkamau@gmail.com>
2022-06-12 09:26:35 +0300

    Add signup form and styling

--- awwards/forms.py
+++ awwards/forms.py
@@ -9,3 +9,8 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+
+        def __init__(self, *args, **kwargs):
+            super(SignUpForm, self).__init__(*args, **kwargs)
+            self.helper = FormHelper()
+            self.helper.form_show_labels = False
