0d646ec5e5dbbf21eb1808783e43131a0743a71a
EmmaK <emmaculatewkamau@gmail.com>
2022-06-12 21:17:38 +0300

    Deploy to heroku

--- AwwardsClone/settings.py
+++ AwwardsClone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['awwards-clone-emz.herokuapp.com','127.0.0.1']
 
 
 # Application definition
